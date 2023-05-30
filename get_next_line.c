d7ed326996b05a559d8b50d1480bdff8ea70bfb1
Carlos De la figuera <carlosdelafiguera@MacBook-Air-de-Carlos.local>
2023-05-30 16:42:52 +0200

    seguimos con el sigsev

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: carlosdelafiguera <carlosdelafiguera@st    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 19:34:04 by carlde-l          #+#    #+#             */
-/*   Updated: 2023/05/26 16:29:45 by carlosdelaf      ###   ########.fr       */
+/*   Updated: 2023/05/30 13:19:13 by carlosdelaf      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 char    *get_next_line(int fd)
 {
-    static char     *stash[257];
+    static char     *stash[OPEN_MAX];
     char            *line;
     
-    if (fd <= 0 || BUFFER_SIZE <= 0 || fd > 256)
+    if (fd <= 0 || BUFFER_SIZE <= 0)
         return (NULL);
     line = NULL;
     stash[fd] = ft_read_and_stash(fd, stash[fd]);
@@ -33,13 +33,13 @@ char    *ft_read_and_stash(int fd, char *stash)
     char            *buf;
     int             bytes_read;
 
-    buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
+    buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
     if (!buf)
         return (NULL);
     bytes_read = 1;
     while (!ft_strchr(stash, '\n') && bytes_read != 0)
     {
-        bytes_read = read(fd, buf, BUFFER_SIZE + 1);
+        bytes_read = read(fd, buf, BUFFER_SIZE);
         if (bytes_read == -1)
         {
             free(buf);
